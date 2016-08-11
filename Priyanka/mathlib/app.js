9d8d8894aac9db6625c779673844d8f07e193e9f
Patrick Todd <patrick.a.todd@gmail.com>
2016-08-11 12:15:14 -0400

    finished dashboard

--- Priyanka/mathlib/app.js
+++
@@ -1,6 +0,0 @@
-var mathlib = require('./mathlib');
-mathlib.greet();
-mathlib.add(5,6);
-mathlib.multiply(2,3);
-mathlib.square(2);
-mathlib.random(1,60);
--- Priyanka/sum.js
+++
@@ -1,5 +0,0 @@
-var sum=0;
-for (var i = 1; i <501; i++) {
-  sum=sum+i;
-}
-console.log(sum);
